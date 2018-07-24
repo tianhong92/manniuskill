6efcc8690d315c133eb7fe0e07fe97c181b2f254
tianhong <tianhong229@gmail.com>
2018-07-24 14:10:55 +0800

    WIP: stream handler

--- src/handler/ManniuStreamHandler.java
+++ src/handler/ManniuStreamHandler.java
@@ -1,21 +1,17 @@
 package handler;
 
-// This is the entry point for AWS Lambda function, it routes all inbound requests to our skill.
-
 import com.amazon.ask.Skill;
 import com.amazon.ask.SkillStreamHandler;
 import com.amazon.ask.Skills;
 
-class ManniuStreamHandler extends SkillStreamHandler {
+public class ManniuStreamHandler extends SkillStreamHandler {
+    public ManniuStreamHandler(Skill skill) {
+        super(skill);
+    }
 
     private static Skill getSkill() {
         return Skills.standard()
                 .addRequestHandlers(new CancelandStopIntentHandler(), new HelloWorldIntentHandler(), new HelpIntentHandler(), new LaunchRequestHandler(), new SessionEndedRequestHandler())
                 .build();
     }
-
-    public ManniuStreamHandler() {
-        super(getSkill());
-    }
-
 }
