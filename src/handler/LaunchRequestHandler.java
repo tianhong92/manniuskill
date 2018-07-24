83a8f786a558ff1545b91db2d4036e8bd2ea6502
tianhong <tianhong229@gmail.com>
2018-07-24 11:04:29 +0800

    add git ignore

--- src/handler/LaunchRequestHandler.java
+++ src/handler/LaunchRequestHandler.java
@@ -1,4 +1,20 @@
 package handler;
 
-public class LaunchRequestHandler {
+public class LaunchRequestHandler implements RequestHandler {
+
+    @Override
+    public boolean canHandle(HandlerInput input) {
+        return input.matches(Predicates.requestType(LaunchRequest.class));
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput input) {
+        String speechText = "Welcome to the Alexa Skills Kit, you can say hello";
+        return input.getResponseBuilder()
+                .withSpeech(speechText)
+                .withSimpleCard("HelloWorld", speechText)
+                .withReprompt(speechText)
+                .build();
+    }
+
 }
