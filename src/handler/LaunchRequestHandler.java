b93d68f5c04f28871c1a5168613286755d67834b
tianhong <tianhong229@gmail.com>
2018-07-24 11:09:03 +0800

    clean git cache

--- src/handler/LaunchRequestHandler.java
+++ src/handler/LaunchRequestHandler.java
@@ -1,5 +1,13 @@
 package handler;
 
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.LaunchRequest;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.request.Predicates;
+
+import java.util.Optional;
+
 public class LaunchRequestHandler implements RequestHandler {
 
     @Override
