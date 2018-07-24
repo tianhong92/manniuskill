da62ae5619c803a0cf7b846476ed149fd5306455
tianhong <tianhong229@gmail.com>
2018-07-24 15:05:14 +0800

    config issue solved

--- src/com/amazon/ask/helloworld/handlers/FallbackIntentHandler.java
+++ src/com/amazon/ask/helloworld/handlers/FallbackIntentHandler.java
@@ -1,4 +1,4 @@
-package handler;
+package com.amazon.ask.helloworld.handlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
@@ -8,20 +8,24 @@ import java.util.Optional;
 
 import static com.amazon.ask.request.Predicates.intentName;
 
-public class HelpIntentHandler implements RequestHandler {
+// 2018-July-09: AMAZON.FallackIntent is only currently available in en-US locale.
+//              This handler will not be triggered except in that locale, so it can be
+//              safely deployed for any locale.
+public class FallbackIntentHandler implements RequestHandler {
 
     @Override
     public boolean canHandle(HandlerInput input) {
-        return input.matches(intentName("AMAZON.HelpIntent"));
+        return input.matches(intentName("AMAZON.FallbackIntent"));
     }
 
     @Override
     public Optional<Response> handle(HandlerInput input) {
-        String speechText = "You can say hello to me!";
+        String speechText = "Sorry, I don't know that. You can say try saying help!";
         return input.getResponseBuilder()
                 .withSpeech(speechText)
                 .withSimpleCard("HelloWorld", speechText)
                 .withReprompt(speechText)
                 .build();
     }
+
 }
