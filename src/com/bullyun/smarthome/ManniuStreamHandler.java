b955b22c412be74fef751f5d003aa1879a915cb2
tianhong <tianhong229@gmail.com>
2018-08-02 11:34:57 +0800

    fix typo

--- src/com/bullyun/smarthome/ManniuStreamHandler.java
+++ src/com/bullyun/smarthome/ManniuStreamHandler.java
@@ -15,40 +15,39 @@ import java.io.OutputStream;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
 public class ManniuStreamHandler implements RequestStreamHandler {
-    private final JacksonSerializer serializer = new JacksonSerializer();;
 
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
         String request = IOUtils.toString(inputStream);
         // 判断请求类型
-        JSONObject jo = JSON.parseObject(request);
-        System.out.println(jo);
-        try {
-            String requestType = jo.getJSONObject("directive").getJSONObject("header").get("name").toString();
-            if(requestType.equals("Discover") || requestType.equals("ReportState")) {
-                String token = jo.getJSONObject("directive").getJSONObject("payload").getJSONObject("scope").get("token").toString();
-                // Get discovers from rest
-            } else if (requestType.equals("InitializeCameraStreams")) {
-                String correlationToken = jo.getJSONObject("directive").getJSONObject("header").get("correlationToken").toString();
-                String token = jo.getJSONObject("directive").getJSONObject("endpoint").getJSONObject("scope").get("token").toString();
-                String endpointId = jo.getJSONObject("directive").getJSONObject("endpoint").get("endpointId").toString();
-                // Get streaming response from rest
-            }
-        } catch (NullPointerException e) {
-            System.out.println("Parse Request: NullPointerException");
-        }
+//        JSONObject jo = JSON.parseObject(request);
+//        System.out.println(jo);
+//        try {
+//            String requestType = jo.getJSONObject("directive").getJSONObject("header").get("name").toString();
+//            if(requestType.equals("Discover") || requestType.equals("ReportState")) {
+//                String token = jo.getJSONObject("directive").getJSONObject("payload").getJSONObject("scope").get("token").toString();
+//                // Get discovers from rest
+//            } else if (requestType.equals("InitializeCameraStreams")) {
+//                String correlationToken = jo.getJSONObject("directive").getJSONObject("header").get("correlationToken").toString();
+//                String token = jo.getJSONObject("directive").getJSONObject("endpoint").getJSONObject("scope").get("token").toString();
+//                String endpointId = jo.getJSONObject("directive").getJSONObject("endpoint").get("endpointId").toString();
+//                // Get streaming response from rest
+//            }
+//        } catch (NullPointerException e) {
+//            System.out.println("Parse Request: NullPointerException");
+//        }
 
 
-//        String respones = TestData.discoveryResponseContent();
-//
-//        LambdaLogger logger = context.getLogger();
-//        logger.log("Get request:");
-//        logger.log(request);
-//        logger.log("Get respones:");
-//        logger.log(respones);
-//        logger.log("end logger!");
-//
-//        byte[] response = respones.getBytes("utf-8");
-//        outputStream.write(response);
+        String response = TestData.discoveryResponseContent();
+
+        LambdaLogger logger = context.getLogger();
+        logger.log("Get request:");
+        logger.log(request);
+        logger.log("Get respones:");
+        logger.log(response);
+        logger.log("end logger!");
+
+        byte[] byteResponse = response.getBytes("utf-8");
+        outputStream.write(byteResponse);
     }
 }
