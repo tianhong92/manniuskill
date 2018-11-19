1d4435d43f1d0de83019c86534e35c6ab941dff7
tianhong229@gmail.com <tianhong229@gmail.com>
2018-11-19 16:22:32 +0800

    tidy up

--- src/main/java/com/bullyun/smarthome/manniuResponse/DeviceList.java
+++ src/main/java/com/bullyun/smarthome/manniuResponse/DeviceList.java
@@ -11,14 +11,10 @@ public class DeviceList {
     private static final String app_secret = "RlA8aCPlsuATT227kKTg003ncP35HYRI";
 
     public static ManniuDeviceList getDeviceList(String access_token) {
-        String testUrl = "http://120.27.148.199:10210/api/v1/devices?app_key=8Wa227sQ00S33p4y&app_secret=RlA8aCPlsuATT227kKTg003ncP35HYRI&access_token=u_ffbf3947071141c9be010cb244fa7ce2";
         String manniuUrl = String.format("%s?app_key=%s&app_secret=%s&access_token=%s", url, app_key, app_secret, access_token);
-        System.out.println(manniuUrl);
-
         String response = HttpClient.doGet(manniuUrl);
         JSONObject json = JSON.parseObject(response);
-        ManniuDeviceList list = json.toJavaObject(ManniuDeviceList.class);
-        return list;
+        return json.toJavaObject(ManniuDeviceList.class);
     }
 
     public static Integer getDeviceState(String access_token,String deviceSn){
